ac24441ddb8b021283591fe032da83af7aa50449
Guillaume Denry <guillaume.denry@gmail.com>
2015-09-03 23:57:22 +0200

    New student button and fields

--- app/components/component.jsx
+++ app/components/component.jsx
@@ -23,6 +23,21 @@ export default React.createClass({
     handleFilterChange(event) {
         this.setState({filter: event.target.value});
     },
+    createStudent() {
+        const firstName = React.findDOMNode(this.refs.firstName).value;
+        const lastName = React.findDOMNode(this.refs.lastName).value;
+
+        $.ajax({
+            method: 'POST',
+            url: 'api/v1/students',
+            contentType: 'application/json',
+    		data: JSON.stringify({ firstName: firstName, lastName: lastName })
+        }).done(data => {
+            this.setState({ users: this.state.users.concat([data])});
+        }).fail((err) => {
+            console.error(err);
+        });
+    },
     userContains(user, c) {
         return user.firstName.toUpperCase().indexOf(c.toUpperCase()) >= 0 || user.lastName.toUpperCase().indexOf(c.toUpperCase()) >= 0;
     },
@@ -30,8 +45,10 @@ export default React.createClass({
         return (
             <div>
                 <h1>Welcome to the ALICE workshops!</h1>
-                <input type='text' onChange={this.handleFilterChange}/>
+                Filter: <input type='text' onChange={this.handleFilterChange}/>
                 {this.state.users.filter(user => this.userContains(user, this.state.filter)).map(user => <User user={user}/>)}
+                <hr/>
+                <span>First name: <input type='text' ref='firstName'/> Last name: <input type='text' ref='lastName'/> <button onClick={this.createStudent}>Add a new student</button></span>
             </div>
         );
     }
