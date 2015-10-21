28ec1f5343c55b9b3a1f5a23170393442fa21821
Guillaume Denry <guillaume.denry@gmail.com>
2015-10-21 18:32:23 +0200

    fix

--- app/components/MainTabBar.jsx
+++ app/components/MainTabBar.jsx
@@ -9,8 +9,8 @@ export default class User extends React.Component {
     }
     render() {
         return (<Tabs activeKey={this.props.currentTab} onSelect={this.props.handleTabSelect}>
-                <Tab eventKey={1} title="Students">Students</Tab>
-                <Tab eventKey={2} title="Time slots">Time slots</Tab>
+                <Tab eventKey={1} title="Students" />
+                <Tab eventKey={2} title="Time slots" />
             </Tabs>
         );
     }
