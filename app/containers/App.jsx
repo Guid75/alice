505b46c1380fa21a7270c35494334f3e05f8664c
Guillaume Denry <guillaume.denry@gmail.com>
2015-10-23 22:36:07 +0200

    Students component becomes a smart container

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -2,27 +2,15 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import Students from '../components/Students.jsx';
+import Students from './Students.jsx';
 import AppHeader from '../components/AppHeader.jsx';
 import MainTabBar from '../components/MainTabBar.jsx';
-import { fetchStudents, filterStudents, addStudent, removeStudent, selectTab } from '../actions';
+import { fetchStudents, selectTab } from '../actions';
 import { List, Map, fromJS } from 'immutable';
 
 var App = React.createClass({
     componentDidMount() {
         this.props.dispatch(fetchStudents());
-    },
-    studentFilterChange(event) {
-        this.props.dispatch(filterStudents(event.target.value));
-    },
-    createStudent(firstName, lastName) {
-        this.props.dispatch(addStudent({
-            firstName,
-            lastName
-        }));
-    },
-    removeStudent(id) {
-        this.props.dispatch(removeStudent(id));
     },
     handleTabSelect(key) {
         this.props.dispatch(selectTab(key));
@@ -31,14 +19,7 @@ var App = React.createClass({
         var mainContain;
         switch (this.props.currentTab) {
             case 'students':
-            mainContain = <Students
-                isFetching={this.props.isFetching}
-                students={this.props.students}
-                studentFilter={this.props.filter}
-                studentFilterChangeHandler={this.studentFilterChange}
-                createStudentHandler={this.createStudent}
-                removeStudentHandler={this.removeStudent}
-                />;
+            mainContain = <Students />;
             break;
             case 'timeslots':
             mainContain = <div/>;
@@ -47,7 +28,6 @@ var App = React.createClass({
             mainContain = <div>Should not happend</div>;
             break;
         }
-
 
         return (
             <div>
@@ -61,10 +41,7 @@ var App = React.createClass({
 
 function mapStateToProps(state) {
   return {
-      currentTab: state.get('currentTab') || 1,
-      students: state.getIn(['students', 'items'], List()),
-      filter: state.getIn(['students', 'filter'], ''),
-      isFetching: state.getIn(['students', 'isFetching'], false)
+      currentTab: state.get('currentTab') || 1
   };
 }
 
---
+++ app/containers/Students.jsx
@@ -0,0 +1,58 @@
+'use strict';
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { Button, Glyphicon } from 'react-bootstrap';
+import User from '../components/User.jsx';
+import { Input } from 'react-bootstrap';
+import { fetchStudents, filterStudents, addStudent, removeStudent } from '../actions';
+import { List } from 'immutable';
+
+class Students extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    studentFilterChangeHandler(event) {
+        this.props.dispatch(filterStudents(event.target.value));
+    }
+    removeStudentHandler(id) {
+        this.props.dispatch(removeStudent(id));
+    }
+    createStudentHandler() {
+        this.props.dispatch(addStudent({
+            firstName: this.refs.firstName.value,
+            lastName: this.refs.lastName.value
+        }));
+    }
+    filterStudent(student) {
+        return student.get('firstName').toUpperCase().indexOf(this.props.filter.toUpperCase()) >= 0 ||
+        student.get('lastName').toUpperCase().indexOf(this.props.filter.toUpperCase()) >= 0 ||
+        (student.get('firstName') + ' ' + student.get('lastName')).toUpperCase().indexOf(this.props.filter.toUpperCase()) >= 0;
+    }
+    render() {
+        const filterStyle = {
+            width: 400,
+            marginLeft: 4
+        };
+        return (
+            <div>
+                <span style={filterStyle}>Filter:</span> <Input type='text' style={filterStyle} value={this.props.studentFilter} onChange={this.studentFilterChangeHandler.bind(this)}/>
+                {this.props.isFetching ?
+                    <span>Fetching students...</span> : this.props.students.filter(this.filterStudent.bind(this)).map(student => <User key={student.get('id')} user={student} removeHandler={this.removeStudentHandler.bind(this)}/>)
+            }
+                <hr/>
+                <span>First name: <input type='text' ref='firstName'/> Last name: <input type='text' ref='lastName'/> <button onClick={this.createStudentHandler.bind(this)}>Add a new student</button></span>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+  return {
+      students: state.getIn(['students', 'items'], List()),
+      filter: state.getIn(['students', 'filter'], ''),
+      isFetching: state.getIn(['students', 'isFetching'], false)
+  };
+}
+
+export default connect(mapStateToProps)(Students);
